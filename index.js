a385d534bd75e19760409255854130e2098bcf1d
chertkovalex <chertkovalex@gmail.com>
2016-05-24 18:47:57 +0300

    task one completed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by Alexanderc on 5/24/2016.
  */
 var url = require('url');
-module.export = {
+module.exports = {
 
     parse: function (str) {
         var result = url.parse(str);
@@ -15,5 +15,5 @@ module.export = {
 
     }
 }
-//module.export.parse("http://www.node.org/docs/index.html");
-//module.export.parse("http://www.node.org:7077/docs/index.html?vasya=petya#myindex");
+//module.exports.parse("http://www.node.org/docs/index.html");
+//module.exports.parse("http://www.node.org:7077/docs/index.html?vasya=petya#myindex");
