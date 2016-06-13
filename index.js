2d061197719983e415a0a8342f4097c52ac4418d
chertkovalex <chertkovalex@gmail.com>
2016-06-13 23:48:54 +0300

    updated validations

--- index.js
+++ index.js
@@ -2,16 +2,29 @@
  * Created by Alexanderc on 5/24/2016.
  */
 var url = require('url');
+var validator = require('validator');
 module.exports = {
 
     parse: function (str) {
         var result = url.parse(str);
 
-        if(typeof result === "object"){
+        if(typeof result === "object"  && validator.isURL(str)){
   //          console.log(result);
-            return result;
-        }else
+
+
+            return {
+                url: result.href,
+                host: result.hostname,
+                port: result.port || 80,
+                file: result.pathname,
+                path: result.path
+            };
+        }else{
+
+            console.log("url: " + str + " invalid");
             return null;
+        }
+
 // some code
     }
 }
